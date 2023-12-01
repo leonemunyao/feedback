afbaa67c484fbdca141d365ac9a8adfa150a00ba
leonemunyao <leonemunyao@gmail.com>
2023-12-01 18:45:44 +0300

    Models.py

--- backend/models.py
+++ backend/models.py
@@ -3,11 +3,15 @@ from django.db import models
 # Feedback models .
 
 class FeedbackCategory(models.Model):
+    # Stores category name
     name = models.CharField(max_length=255)
 
     def __str__(self):
+        # Returns category name as string
         return self.name
-
+    
+# This is the Feedback model
+# It contains category, message, attachment, and timestamp fields
 class Feedback(models.Model):
     category = models.ForeignKey(FeedbackCategory, on_delete=models.CASCADE)
     message = models.TextField()
